3a4ec207772a24af63ca5ba19c6dd46bc95022fd
Taycamgame <taycamgame01@gmail.com>
2024-12-01 17:25:45 +0000

    Setup project

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode2024.puzzlesolutions.day1;
+
+namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1Part1 day1 = new Day1Part1();
+            day1.printAnswer();
+        }
+    }
+}
---
+++ AdventOfCode2024/puzzlesolutions/day1/Day1Part1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.puzzlesolutions.day1
+{
+    class Day1Part1
+    {
+        public void printAnswer()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "puzzlesolutions\\day1\\day1part1.txt");
+            string input = File.ReadAllText(path);
+            Console.WriteLine(input);
+        }
+    }
+}
